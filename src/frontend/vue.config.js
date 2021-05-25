a42bd9a9ca5d6c35e9ff46dde3f974587cda7330
jonghyeok34 <jonghyeok34@finotek.co.kr>
2021-05-25 15:59:50 +0900

    change environment for dev

--- src/frontend/vue.config.js
+++ src/frontend/vue.config.js
@@ -1,18 +1,16 @@
 const path = require("path");
 
 module.exports = {
- 
   outputDir: path.resolve(__dirname, "./build"),
 
   devServer: {
     overlay: false,
     proxy: {
-      
       "/api": {
         target: process.env.VUE_APP_ADMIN_API_URL,
         ws: true,
         changeOrigin: true
-      },
+      }
     }
   },
   transpileDependencies: ["vuetify"]
